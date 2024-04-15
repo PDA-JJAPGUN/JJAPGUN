a646b66c691ef4e96711e8b135231538235f024e
youhyeoneee <khukyh2018@khu.ac.kr>
2024-04-15 12:06:43 +0900

    feat: add game start button to GamtTitle

---
+++ src/view/GameFrame.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame implements ScreenSize {
+    private GameFrame gameFrame = this;
+
+    public GameFrame() {
+        setTitle("JJAP GUN");
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+
+        init();
+    }
+
+    public void init() {
+        change("gameTitle");
+    }
+    // 패널 바꾸기 함수
+    public void change(String panelName) {
+        if (panelName.equals("gameTitle")) {
+            GameTitle gameTitle = new GameTitle(gameFrame);
+            getContentPane().removeAll();
+            getContentPane().add(gameTitle);
+            revalidate();
+            repaint();
+        }
+    }
+
+}
