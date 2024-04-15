5752bed209adc2a12bade5c834de659b846c81a0
Youjun Seo <76039485+SEO-YJ@users.noreply.github.com>
2024-04-15 21:07:28 +0900

    feat: add UserController

---
+++ src/controller/UserController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import dto.LoginDto;
+import dto.SignupDto;
+import service.UserService;
+import view.UserView;
+
+public class UserController {
+    UserService userService;
+
+    // 모델 객체의 메소드 호출
+    public UserController(UserService userService) {
+        this.userService = userService;
+        new UserView(this);
+    }
+
+    // 메소드 -> 모델.plus();
+    // 자바에서 값을 전달하는 방법은 '매개변수' 밖에 없다.
+    public String signup(SignupDto signUpDto) {
+        return userService.signup(signUpDto);
+    }
+
+    // 로그인
+    public Boolean login(LoginDto loginDto) {
+        return userService.login(loginDto);
+    }
+}
