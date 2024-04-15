d4f692d0b1e8850e5af841fc8bb04565bdd49cf3
youhyeoneee <khukyh2018@khu.ac.kr>
2024-04-15 14:35:38 +0900

    refactor: use panel enum

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -6,7 +6,8 @@ import javax.swing.*;
 public class GameFrame extends JFrame implements ScreenSize {
     private GameFrame gameFrame = this;
     public GameMap gameMap;
-    public GameTitle gameTitle;
+    public GameStart gameStart;
+    public GameEnd gameEnd;
     public boolean isgame;
 
     public GameFrame() {
@@ -20,23 +21,32 @@ public class GameFrame extends JFrame implements ScreenSize {
     }
 
     public void init() {
-        change("gameTitle");
+        change(Panel.GAME_START.name());
+//        change(Panel.GAME_END.name());
     }
+
     // 패널 바꾸기 함수
     public void change(String panelName) {
-        if (panelName.equals("gameTitle")) {
-            gameTitle = new GameTitle(gameFrame);
-            getContentPane().removeAll();
-            getContentPane().add(gameTitle);
-            revalidate();
-            repaint();
-        } else if(panelName.equals("gameMap")){
-            gameMap = new GameMap(gameFrame);
-            getContentPane().removeAll();
-            getContentPane().add(gameMap);
-            revalidate();
-            repaint();
+
+        getContentPane().removeAll();
+
+        switch (Panel.valueOf(panelName)) {
+            case GAME_START:
+                gameStart = new GameStart(gameFrame);
+                getContentPane().add(gameStart);
+                break;
+            case GAME_MAP:
+                gameMap = new GameMap(gameFrame);
+                getContentPane().add(gameMap);
+                break;
+            case GAME_END:
+                gameEnd = new GameEnd(gameFrame);
+                getContentPane().add(gameEnd);
+                break;
         }
+
+        revalidate();
+        repaint();
     }
 
 }
