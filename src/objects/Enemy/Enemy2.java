a2a819770198fdf7bae297822efcbad7b6e234b2
eunxoo <dmstn7989@naver.com>
2024-04-16 09:40:03 +0900

    feat: enemy 모델

---
+++ src/objects/Enemy/Enemy2.java
@@ -0,0 +1,130 @@
+package objects.Enemy;
+
+import objects.Bullet;
+import objects.Enemy.Enemy;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+public class Enemy2 extends Enemy {
+
+    private Enemy2 enemy2 = this;
+    private static final String TAG = "Enemy4 : ";
+
+//    ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+//    private Bullet bullet;
+
+    public Enemy2(int x, int y, int w, int h) {
+//        this.player = player;
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.image = new ImageIcon("images/enemy4.png").getImage();
+        this.life = 3;
+        this.crushCheck = false;
+        this.isThreadLife = true;
+
+//        this.player.contextAdd(enemy2);
+
+        this.move();
+//        this.crush();
+
+    }
+
+    public void move() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                count = 0;
+                while (isThreadLife) {
+                    try {
+                        Thread.sleep(5);
+
+                        movedown();
+                        y++; // down가속
+
+                        if (y < 400) {
+                            moveright();
+
+                        }
+
+//                        bulletCreate();
+                        count++;
+
+                        if (y > 900) {
+                             System.out.println("enemy4 쓰레드 종료");
+                            isThreadLife =false;
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+//    public void crush() { // 적비행기-Player 충돌
+//
+//        new Thread(new Runnable() {
+//
+//            @Override
+//            public void run() {
+//
+//                while (!player.getInvincible() && y < 900 && y > -300) {
+//
+//                    if (Math.abs((player.getX() + player.getWidth() / 2) - (x + player.getWidth() / 2)) < (width / 2
+//                            + player.getWidth() / 2)
+//                            && Math.abs((player.getY() + player.getHeight() / 2) - (y + height / 2)) < (height / 2
+//                            + player.getHeight() / 2)) {
+//                        collision = true;
+//                    } else {
+//                        collision = false;
+//                    }
+//
+//                    try {
+//                        if (collision) {
+//                            exploseEnemy(player, enemy2); // 충돌 폭발 메서드
+//                        }
+//                        Thread.sleep(10);
+//
+//                        if (crushCheck) {
+//                            exploseEnemy(enemy2);
+//                        }
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//
+//            }
+//        }).start();
+//
+//    }
+
+
+
+//    public void bulletCreate() {
+//        if (count % 100 == 0) {
+//            bullet = new Bullet(x + 20, y + 40, 270, 4, 20, 20);
+//            bullets.add(bullet);
+//
+//        }
+//    }
+
+    @Override
+    public void planeDraw(Graphics g) { // 그림그리기
+        g.drawImage(image, x, y, width, height, null);
+//        for (int i = 0; i < bullets.size(); i++) {
+//            bullet = bullets.get(i);
+//            g.drawImage(bullet.bulletImg1, bullet.getX(), bullet.getY(), bullet.getWidth(),
+//                    bullet.getHeight(), null);
+//
+//
+//        }
+    }
+
+}
--- src/view/GameMap.java
+++ src/view/GameMap.java
@@ -1,9 +1,15 @@
 package view;
 
 import javax.swing.*;
+
 import objects.Boss;
+import objects.Enemy.Enemy;
+import objects.Enemy.Enemy1;
+import objects.Enemy.Enemy2;
+import objects.Enemy.Enemy3;
 
 import java.awt.*;
+import java.util.Vector;
 
 public class GameMap extends JPanel {
 
@@ -11,6 +17,10 @@ public class GameMap extends JPanel {
     private GameMap gameMap = this;
     private GameFrame gameFrame;
     private Boss boss;
+
+    Vector<Enemy> enemyUnits = new Vector<>();
+
+    Enemy enemy;
     private ImageIcon bossStageIcon = new ImageIcon("images/vsBossStage.png");
     private Image bossStageImg = bossStageIcon.getImage();
     private ImageIcon stageIcon = new ImageIcon("images/Stage.png");
@@ -18,14 +28,14 @@ public class GameMap extends JPanel {
     int stageY1 = -stageImg.getHeight(null) + bossStageImg.getHeight(null);
     int stageY2 = -stageImg.getHeight(null) + bossStageImg.getHeight(null);
     int bossStageBY1 = -bossStageImg.getHeight(null);
-    int bossStageBY2 = -bossStageImg.getHeight(null)*2;
+    int bossStageBY2 = -bossStageImg.getHeight(null) * 2;
 
     int appear = 1;
     int score = 0;
     JLabel la_score;
-    Font font = new Font(null,1,40);
+    Font font = new Font(null, 1, 40);
 
-    public GameMap(GameFrame gameFrame){
+    public GameMap(GameFrame gameFrame) {
 
         this.gameFrame = gameFrame;
 
@@ -35,15 +45,15 @@ public class GameMap extends JPanel {
                 gameFrame.isgame = true;
                 setLayout(null);
 
-                la_score = new JLabel(score+"");
+                la_score = new JLabel(score + "");
                 la_score.setForeground(Color.WHITE);
                 la_score.setFont(font);
                 la_score.setBounds(20, 0, 300, 100);
                 add(la_score);
-                setComponentZOrder(la_score,0);
+                setComponentZOrder(la_score, 0);
 
-                while(gameFrame.isgame){
-                    if(boss == null){
+                while (gameFrame.isgame) {
+                    if (boss == null) {
                         stageY1++;
                         stageY2++;
                         if (stageY1 > stageImg.getHeight(null)) {
@@ -53,9 +63,9 @@ public class GameMap extends JPanel {
                             stageY2 = -stageImg.getHeight(null);
                         }
                     }
-                    if(boss != null){
-                        if(bossStageBY1 < bossStageImg.getHeight(null) &&
-                                bossStageBY2 < bossStageImg.getHeight(null)){
+                    if (boss != null) {
+                        if (bossStageBY1 < bossStageImg.getHeight(null) &&
+                                bossStageBY2 < bossStageImg.getHeight(null)) {
                             stageY1++;
                             stageY2++;
                         }
@@ -69,20 +79,22 @@ public class GameMap extends JPanel {
                         }
                     }
 
-                    try{
+                    try {
                         appear++;
+                        System.out.println(appear);
                         batchEnemy();
                         repaint();
                         Thread.sleep(3);
-                    }catch(Exception e){
+                    } catch (Exception e) {
                         //Handle exception
                     }
                 }
             }
         }).start();
     }
+
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         g.drawImage(stageImg, 0, stageY1, null);
@@ -90,30 +102,45 @@ public class GameMap extends JPanel {
         g.drawImage(bossStageImg, 0, bossStageBY1, null);
         g.drawImage(bossStageImg, 0, bossStageBY2, null);
 
+        for (int i = 0; i < enemyUnits.size(); i++) { // null이 아니면 그려라
+            if (enemyUnits.get(i) != null) {
+                enemyUnits.get(i).planeDraw(g);
+            }
+        }
 
-        if (boss != null){
+        if (boss != null) {
             boss.bossDraw(g);
         }
 
         repaint();
     }
 
-    public void batchEnemy(){
-        if(appear == 3000){
+    public void batchEnemy() {
+        if (boss == null) {
+            if(appear % 3000 == 0){
+                enemyUnits.add(new Enemy3(600, -200, 100, 100)); // 컨텍스트 넘기기
+                enemyUnits.add(new Enemy2(0, 0, 100, 100));
+            }
+            if (appear % 2000 == 1000 && appear % 3000 != 0) {
+                enemyUnits.add(new Enemy1(50, 0, 50, 50));
+                enemyUnits.add(new Enemy1(100, -50, 50, 50));
+                enemyUnits.add(new Enemy1(150, -100, 50, 50));
+                enemyUnits.add(new Enemy1(200, -150, 50, 50));
+                enemyUnits.add(new Enemy1(250, -200, 50, 50));
+            }
+            if (appear % 2000 == 0 && appear % 3000 != 0){
+                enemyUnits.add(new Enemy1(500, 0, 50, 50));
+                enemyUnits.add(new Enemy1(450, -50, 50, 50));
+                enemyUnits.add(new Enemy1(400, -100, 50, 50));
+                enemyUnits.add(new Enemy1( 350, -150, 50, 50));
+                enemyUnits.add(new Enemy1(300, -200, 50, 50));
+            }
+        }
+
+        if (appear == 10000) {
             boss = new Boss(0, -300);
         }
-        if(appear == 2000*30){
-            boss = new Boss(0, -300);
-        }
-        if(appear == 3000*30){
-            boss = new Boss(0, -300);
-        }
-        if(appear == 4000*30){
-            boss = new Boss(0, -300);
-        }
-        if(appear == 5000*30){
-            boss = new Boss(0, -300);
-        }
+
     }
 }
 
